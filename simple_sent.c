12746d01c7d813b2c32a391732cbc89067a796c3
Rakesh_S <srakesh1996@outlook.com>
2018-09-12 09:36:50 -0700

    Update simple_sent.c

--- simple_sent.c
+++ simple_sent.c
@@ -1,9 +1,8 @@
 /* This example demonstrate sending single 
-	character 'A' using spi protocol in PIC16F877A 
+	character 'A' using spi protocol (Master mode) in PIC16F877A 
 */
 
 #include<pic.h>
-#define SS RC2
 void delay(int a)
 {
 	int i,j;
@@ -17,10 +16,7 @@ void spi_init()
 }
 void spi_sent(b)
 {
-	SS = 0;
 	SSPBUF = b;
-	delay(200);
-	SS = 1;
 	delay(200);
 }	
 int main()
@@ -29,7 +25,9 @@ int main()
 	spi_init();
 	while(1)
 	{
-		spi_sent('A');
+		spi_sent('B');
+		while(WCOL == 1);
+		WCOL = 0;
 		delay(300);
 	}
 }
